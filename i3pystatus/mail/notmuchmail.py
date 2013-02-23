93b7ba9af94c0224e9cb8b1ca508335ae48e527a
enkore <public@enkore.de>
2013-02-23 15:16:53 +0100

    Ported notmuchmail (needs testing)

---
+++ i3pystatus/mail/notmuchmail.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# note that this needs the notmuch python bindings. For more info see:
+# http://notmuchmail.org/howto/#index4h2
+import notmuch
+import json
+
+from i3pystatus.mail import Backend
+
+class NotmuchMailChecker(Backend):
+    """
+    This class uses the notmuch python bindings to check for the
+    number of messages in the notmuch database with the tags "inbox"
+    and "unread"
+    """
+
+    settings = required = ("db_path",)
+
+    def init(self):
+        self.db = notmuch.Database(self.db_path)
+
+    @property
+    def unread(self):
+        return notmuch.Query(self.db, "tag:unread and tag:inbox").count_messages()
