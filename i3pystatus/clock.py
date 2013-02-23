70155b2f182406ea9cf9e0d840aa0e393b394746
enkore <public@enkore.de>
2013-02-23 19:05:57 +0100

    Fixed clock (my bad)

--- i3pystatus/clock.py
+++ i3pystatus/clock.py
@@ -17,7 +17,7 @@ class Clock(IntervalModule):
         lang = os.environ.get('LANG', None)
         if lang:
             locale.setlocale(locale.LC_ALL, lang)
-        if self.format is not None:
+        if self.format is None:
             lang = locale.getlocale()[0]
             if lang == 'en_US':
                 # MDY format - United States of America
