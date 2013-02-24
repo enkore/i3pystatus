20da1dbb08570157647ea30f7c5fa1b2cbc68cef
enkore <public@enkore.de>
2013-02-24 05:20:35 +0100

    Remove backlight module, add file module (kinda overpowered :-)

---
+++ i3pystatus/file.py
@@ -0,0 +1,47 @@
+from os.path import join
+
+from i3pystatus import IntervalModule
+
+class File(IntervalModule):
+    """
+    Rip information from text files
+
+    components is a dict of pairs of the form:
+
+        name => (callable, file)
+
+    * Where `name` is a valid identifier, which is used in the format string to access
+    the value of that component.
+    * `callable` is some callable to convert the contents of `file`. A common choice is
+    float or int.
+    * `file` names a file, relative to `base_path`.
+
+    transform is a optional dict of callables taking a single argument, a dictionary containing the values
+    of all components. The return value is bound to `name`
+    """
+    settings = (
+        ("format", "format string"),
+        ("components", "List of tripels"),
+        ("transforms", "List of pairs"),
+        ("base_path", ""),
+        "color"
+    )
+    required = ("format", "components")
+    base_path = "/"
+    transforms = tuple()
+    color = "#FFFFFF"
+
+    def run(self):
+        cdict = {}
+
+        for key, (component, file) in self.components.items():
+            with open(join(self.base_path, file), "r") as f:
+                cdict[key] = component(f.read().strip())
+
+        for key, transform in self.transforms.items():
+            cdict[key] = transform(cdict)
+
+        self.output = {
+            "full_text": self.format.format(**cdict),
+            "color": self.color
+        }
