2e9b80c0a09cc4883f6d5ca78424d67abca7b037
enkore <public@enkore.de>
2013-03-08 20:37:40 +0100

    Oops! Forgot to check in the actual code for wireless module
    (3.8.2)

---
+++ i3pystatus/wireless.py
@@ -0,0 +1,36 @@
+
+import basiciw
+
+from i3pystatus.network import Network
+
+class Wireless(Network):
+    """
+    Display network information about a interface.
+
+    Requires the PyPI packages `netifaces-py3` and `basiciw`.
+
+    This is based on the network module, so all options and formatters are
+    the same, except for these additional formatters:
+    * {essid} ESSID of currently connected wifi
+    * {freq} Current frequency
+    * {quality} Link quality in percent
+    """
+
+    def collect(self):
+        color, format, fdict, up = super().collect()
+
+        if up:
+            iwi = basiciw.iwinfo(self.interface)
+            fdict["essid"] = iwi["essid"]
+            fdict["freq"] = iwi["freq"]
+            quality = iwi["quality"]
+            if quality["quality_max"] > 0:
+                fdict["quality"] = quality["quality"] / quality["quality_max"]
+            else:
+                fdict["quality"] = quality["quality"]
+            fdict["quality"] *= 100
+        else:
+            fdict["essid"] = ""
+            fdict["freq"] = fdict["quality"] = 0.0
+
+        return (color, format, fdict, up)
