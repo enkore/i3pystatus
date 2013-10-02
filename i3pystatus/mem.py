9c924f77e9b7fb23a2a1f7d828908a64d328273d
enkore <public@enkore.de>
2013-10-03 00:52:56 +0200

    mem: docstring list, pep8 #16

--- i3pystatus/mem.py
+++ i3pystatus/mem.py
@@ -4,24 +4,30 @@ from psutil import virtual_memory
 class Mem(IntervalModule):
     """
     Shows memory load
+
     Available formatters:
-    {avail_mem}
-    {percent_used_mem}
-    {used_mem}
-    {total_mem}
+    * {avail_mem}
+    * {percent_used_mem}
+    * {used_mem}
+    * {total_mem}
     """
 
     format = "{avail_mem} MB"
     settings = (
-        ("format", "format string used for output. {free_mem is the amount of free memory in MB}."),
+        ("format", "format string used for output."),
     )
 
     def run(self):
-        avail_mem = int(round(virtual_memory().available/1024,0))
-        used_mem = int(round(virtual_memory().used/1024,0))
-        percent_used_mem = int(round(virtual_memory().percent/1024,0))
-        total_mem = int(round(virtual_memory().total/1024,0))
+        vm = virtual_memory()
+        avail_mem = int(round(vm.available/1024, 0))
+        used_mem = int(round(vm.used/1024, 0))
+        percent_used_mem = int(round(vm.percent/1024, 0))
+        total_mem = int(round(vm.total/1024, 0))
         #free_swap = int(round(phymem_usage().free/1024,0))
         self.output = {
-            "full_text" : self.format.format(used_mem=used_mem, avail_mem=avail_mem, total_mem=total_mem, percent_used_mem=percent_used_mem)
+            "full_text" : self.format.format(
+                used_mem=used_mem,
+                avail_mem=avail_mem,
+                total_mem=total_mem,
+                percent_used_mem=percent_used_mem)
         }
