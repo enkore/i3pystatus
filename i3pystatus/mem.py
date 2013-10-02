a63cfa20e154017b026f2e26b22cad06aaf2e0e0
Cezary Biele <cezary.biele@gmail.com>
2013-10-02 23:14:13 +0200

    changed mem.py to use psutil

--- i3pystatus/mem.py
+++ i3pystatus/mem.py
@@ -1,20 +1,30 @@
 from i3pystatus import IntervalModule
+from psutil import virtual_memory
 
 class Mem(IntervalModule):
     """
-    Shows system load
+    Shows memory load
+    Available formatters:
+    {avail_mem}
+    {percent_used_mem}
+    {used_mem}
+    {total_mem}
     """
 
-    format = "{free_mem} MB"
+    format = "{avail_mem} MB, {used_mem}"
     settings = (
         ("format", "format string used for output. {free_mem is the amount of free memory in MB}."),
     )
 
-    file = "/proc/meminfo"
-
     def run(self):
-        with open(self.file, "r") as f:
-            free_mem = int(round(int(f.readlines()[1].split()[1])/1024,0))
+        avail_mem = int(round(virtual_memory().available/1024,0))
+        used_mem = int(round(virtual_memory().used/1024,0))
+        percent_used_mem = int(round(virtual_memory().percent/1024,0))
+        total_mem = int(round(virtual_memory().total/1024,0))
+        #free_swap = int(round(phymem_usage().free/1024,0))
         self.output = {
-            "full_text" : self.format.format(free_mem=free_mem),
+            "full_text" : self.format.format(used_mem=used_mem, avail_mem=avail_mem, total_mem=total_mem, percent_used_mem=percent_used_mem)
         }
+        print(self.output)
+a=Mem()
+a.run()
