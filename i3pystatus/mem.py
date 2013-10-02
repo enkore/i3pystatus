7f8ae4fedf59a4fd14bb799a19b0202822c0493a
enkore <public@enkore.de>
2013-10-03 00:56:33 +0200

    mem: correct divider to megabyte (was kilobyte), don't divide percentage #16

--- i3pystatus/mem.py
+++ i3pystatus/mem.py
@@ -1,6 +1,8 @@
 from i3pystatus import IntervalModule
 from psutil import virtual_memory
 
+MEGABYTE = 1024**2
+
 class Mem(IntervalModule):
     """
     Shows memory load
@@ -19,15 +21,10 @@ class Mem(IntervalModule):
 
     def run(self):
         vm = virtual_memory()
-        avail_mem = int(round(vm.available/1024, 0))
-        used_mem = int(round(vm.used/1024, 0))
-        percent_used_mem = int(round(vm.percent/1024, 0))
-        total_mem = int(round(vm.total/1024, 0))
-        #free_swap = int(round(phymem_usage().free/1024,0))
         self.output = {
             "full_text" : self.format.format(
-                used_mem=used_mem,
-                avail_mem=avail_mem,
-                total_mem=total_mem,
-                percent_used_mem=percent_used_mem)
+                used_mem=int(round(vm.used/MEGABYTE, 0)),
+                avail_mem=int(round(vm.available/MEGABYTE, 0)),
+                total_mem=int(round(vm.total/MEGABYTE, 0)),
+                percent_used_mem=vm.percent)
         }
