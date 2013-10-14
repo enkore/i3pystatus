dc85b646313f148093bf3b12b9d312b789a385d1
Cezary Biele <cezary.biele@gmail.com>
2013-10-14 11:54:25 +0200

    added humidity to weather module

--- i3pystatus/weather.py
+++ i3pystatus/weather.py
@@ -4,32 +4,31 @@ import pywapi
 class Weather(IntervalModule):
 
     """
-    This module gets the weather from weather.com
-    First, you need to get the code for the location from the website
+    This module gets the weather from weather.com using pywapi module
+    First, you need to get the code for the location from the www.weather.com
     Available formatters:
-        {temp}
+        {current_temp}
+        {humidity}
     """
 
     interval = 20
 
     settings = (
         "location_code",
+        "units",
         "format",
     )
-    required = ("location_code")
-
-    location_code='PLXX0028'
-
-    format = "{current_temp}°C"
+    #required = ("location_code")
+    format = "{current_temp}"
 
     def run(self):
-        current_temp = pywapi.get_weather_from_weather_com(self.location_code)['current_conditions']['temperature']
+        #current_temp = pywapi.get_weather_from_weather_com(self.location_code)['current_conditions']['temperature']
+        result = pywapi.get_weather_from_weather_com(self.location_code, self.units)
+        conditions = result['current_conditions']
+        temperature = conditions['temperature']
+        humidity = conditions['humidity']
+        units = result['units']
+        current_temp = '{t}°{d}'.format(t=temperature, d=units['temperature'])
         self.output = {
-            "full_text": self.format.format(current_temp=current_temp)
+            "full_text": self.format.format(current_temp=current_temp, humidity=humidity)
         }
-        #print ( self.output )
-
-#w=Weather()
-#w.run()
-    #def on_leftclick(self):
-        #webbrowser.open_new_tab(self.instance.get_url())
