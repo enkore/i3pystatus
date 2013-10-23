1295bf1ec5eea129f1d62e41970e2dbd33469e5d
enkore <public@enkore.de>
2013-10-23 21:52:44 +0200

    exceptions: define format in ABC

--- i3pystatus/core/exceptions.py
+++ i3pystatus/core/exceptions.py
@@ -1,6 +1,5 @@
 
 class ConfigError(Exception):
-
     """ABC for configuration exceptions"""
 
     def __init__(self, module, *args, **kwargs):
@@ -9,26 +8,25 @@ class ConfigError(Exception):
 
         super().__init__(message)
 
+    def format(self, *args, **kwargs):
+        return ""
+
 
 class ConfigKeyError(ConfigError, KeyError):
-
     def format(self, key):
         return "invalid option '{0}'".format(key)
 
 
 class ConfigMissingError(ConfigError):
-
     def format(self, missing):
         return "missing required options: {0}".format(missing)
 
 
 class ConfigAmbigiousClassesError(ConfigError):
-
     def format(self, ambigious_classes):
         return "ambigious module specification, found multiple classes: {0}".format(ambigious_classes)
 
 
 class ConfigInvalidModuleError(ConfigError):
-
     def format(self):
         return "no class found"
