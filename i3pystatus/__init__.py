a5b57601da6e9b85eca18d61e3784addd1863fa4
enkore <public@enkore.de>
2013-10-30 10:35:24 +0100

    Make i3pystatus a namespace package

--- i3pystatus/__init__.py
+++ i3pystatus/__init__.py
@@ -1,6 +1,9 @@
-#!/usr/bin/env python
+
+from pkgutil import extend_path
 
 from i3pystatus.core import Status
 from i3pystatus.core.modules import Module, IntervalModule
 from i3pystatus.core.settings import SettingsBase
 from i3pystatus.core.util import formatp
+
+__path__ = extend_path(__path__, __name__)
