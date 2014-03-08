2889a61060ffb23f6fe766a9eb03ccd99e913f91
siikamiika <siikamiika@users.noreply.github.com>
2014-03-08 17:56:17 +0200

    test script removed

--- i3pystatus/test.py
+++
@@ -1,6 +0,0 @@
-from i3pystatus import Status
-status = Status(standalone=True)
-import mpd
-status.register(mpd.MPD, format="[({song_elapsed}/{song_length})] [{artist} - ]{title} {status} ♪{volume}", status={"pause": "▷", "play": "▶", "stop": "◾",})
-status.run()
-
