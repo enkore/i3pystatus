d1725228d0136463bca66d7bcb383aae32866636
enkore <public@enkore.de>
2014-10-14 16:44:40 +0200

    PEP 8 (I'm bored): uptime

--- i3pystatus/uptime.py
+++ i3pystatus/uptime.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 from i3pystatus import IntervalModule, formatp
 from i3pystatus.core.util import TimeWrapper
+
 
 class Uptime(IntervalModule):
     """
@@ -10,25 +9,25 @@ class Uptime(IntervalModule):
     """
 
     settings = (
-        ("format","Format string"),
-        ("color","String color"),
-        ("alert","If you want the string to change color"),
-        ("seconds_alert","How many seconds necessary to start the alert"),
-        ("color_alert","Alert color"),
+        ("format", "Format string"),
+        ("color", "String color"),
+        ("alert", "If you want the string to change color"),
+        ("seconds_alert", "How many seconds necessary to start the alert"),
+        ("color_alert", "Alert color"),
     )
 
-    file           = "/proc/uptime"
-    format         = "up {uptime}"
-    color          = "#ffffff"
-    alert          = False
-    seconds_alert  = 3600
-    color_alert    = "#ff0000"
+    file = "/proc/uptime"
+    format = "up {uptime}"
+    color = "#ffffff"
+    alert = False
+    seconds_alert = 3600
+    color_alert = "#ff0000"
 
     def run(self):
-        with open(self.file,'r') as f:
+        with open(self.file, "r") as f:
             seconds = float(f.read().split()[0])
             fdict = {
-                "uptime" : TimeWrapper(seconds, "%h:%m"),
+                "uptime": TimeWrapper(seconds, "%h:%m"),
             }
 
         if self.alert:
