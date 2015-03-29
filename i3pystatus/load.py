21ce8db1203dfb22039dd829280b141f1b0e1190
Arvedui <asseon@posteo.de>
2015-03-29 12:24:09 +0200

    adjusted docs

--- i3pystatus/load.py
+++ i3pystatus/load.py
@@ -12,7 +12,7 @@ class Load(IntervalModule):
         ("format",
          "format string used for output. {avg1}, {avg5} and {avg15} are the load average of the last one, five and fifteen minutes, respectively. {tasks} is the number of tasks (i.e. 1/285, which indiciates that one out of 285 total tasks is runnable)."),
         ("color", "The text color"),
-        ("critical_limit", "Limit above which the load is considered critical"),
+        ("critical_limit", "Limit above which the load is considered critical, defaults to amount of cores."),
         ("critical_color", "The critical color"),
     )
 
