4d24b17e2b25e5e336bda37eaf3a06846b38b417
enkore <public@enkore.de>
2015-10-25 20:30:36 +0100

    Fix crash on import with desktop notifications when python-gobject is available, but no notification daemon is running.

--- i3pystatus/core/desktop.py
+++ i3pystatus/core/desktop.py
@@ -33,7 +33,7 @@ try:
     import gi
     gi.require_version('Notify', '0.7')
     from gi.repository import Notify
-except ImportError:
+except ImportError, ValueError:
     pass
 else:
     if not Notify.init("i3pystatus"):
