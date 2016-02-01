d018be872a366bdaa1feddb5460fb30880af27e1
enkore <public@enkore.de>
2016-02-01 12:18:03 +0100

    Lift restriction that "run" cannot be used as a callback

--- i3pystatus/core/modules.py
+++ i3pystatus/core/modules.py
@@ -101,9 +101,8 @@ class Module(SettingsBase):
         elif our_method:
             cb(self, *args)
         elif hasattr(self, cb):
-            if cb is not "run":
-                self.__log_button_event(button, cb, args, "Member callback")
-                getattr(self, cb)(*args)
+            self.__log_button_event(button, cb, args, "Member callback")
+            getattr(self, cb)(*args)
         else:
             self.__log_button_event(button, cb, args, "External command")
             if hasattr(self, "data"):
