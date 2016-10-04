feedd91917b872aff19fd18aab0b8b0450a39067
Matthieu Coudron <mattator@gmail.com>
2016-10-04 23:23:38 +0200

    Removed calendar selection

--- i3pystatus/khal_calendar.py
+++ i3pystatus/khal_calendar.py
@@ -6,9 +6,7 @@ from i3pystatus.core.util import user_open, internet, require
 import khal
 import khal.settings
 import khal.cli
-from  datetime import datetime, date
-
-
+from datetime import datetime, date, timedelta
 
 
 class KhalCalendar(IntervalModule):
@@ -16,57 +14,37 @@ class KhalCalendar(IntervalModule):
 
     .. rubric:: Available formatters
 
-    * `{calendar}`        —  Current calendar
+    * `{calendars}`        —  Current calendar
     * `{nb_events}`  — number of events for the day
-
-        See http://khal.readthedocs.io/en/latest/usage.html#cmdoption--format
+    * `{next_event}`  —  Display next event summary
     """
 
-    max_error_len = 50
     color = '#78EAF2'
-    format = '{calendar} / {nb_events}'
+    format = '{nb_events} event(s) {next_event}'
     interval = 1
 
     current_calendar = None
     config_filename = None
     on_rightclick = 'termite -e ikhal'
 
-    on_upscroll = ["cycle_through_calendars", 1]
-    on_downscroll = ["cycle_through_calendars", -1]
+    calendars = None
+    days = 1
 
     settings = (
         ('config_filename', "Path to your khal.conf"),
         ('format', 'Format string'),
-        ("calendar", "calendar name"),
-        ("next_event", "see https://developer.github.com/v3/#authentication"),
-        ("nb_events", "Number of events within the selection")
+        ("calendars", "Restrict to these calendars pass as a list) "),
+        ("days", "Check for the next X days"),
     )
 
     def init(self):
         self.config = khal.settings.get_config(self.config_filename)
         self.collection = None
-        self.current_calendar = None
-
-        # CalendarCollection is defined in khalendar.py
-    def cycle_through_calendars(self, step=1):
-        # from itertools import cycle
-
-
-        # print(self.config["calendars"])
-        self.logger.info("hello world %s" % self.config["calendars"])
-        try:
-            calendars = self.config["calendars"].keys()
-            idx = calendars.index(self.current_calendar)
-            self.current_calendar  = calendars[ idx + step % len(calendars) ]
-            self.logger.info("Newly selected calender %s" % self.current_calendar)
-        except ValueError:
-            self.current_calendar = self.collection.default_calendar_name
-
 
     def open_connection(self,):
         self.logger.debug("Opening collection")
-        self.collection = khal.cli.build_collection(self.config, None,) 
-        self.current_calendar = self.collection.default_calendar_name
+        self.collection = khal.cli.build_collection(self.config, None,)
+        self.logger.debug("Available calendars=%s" % self.collection.names)
 
     def run(self):
 
@@ -76,10 +54,22 @@ class KhalCalendar(IntervalModule):
             self.open_connection()
 
         self.logger.info("Running")
-        format_values['calendar'] = self.current_calendar
-        events = list(self.collection.get_events_on( date.today() ))
+
+        # retrieve events for the next X days
+        events = []
+        for days in range(self.days):
+            events += list(self.collection.get_events_on(
+                date.today() + timedelta(days=days))
+            )
+
+        # filter out unwanted calendars
+        self.logger.debug("calendars %s" % self.calendars)
+        if self.calendars is not None:
+            events = [evt for evt in events if evt.calendar in self.calendars]
 
         if len(events):
+            # We could probably add an option see
+            # http://khal.readthedocs.io/en/latest/usage.html#cmdoption--format
             format_values['next_event'] = events[0].summary
 
         format_values['nb_events'] = len(events)
