bdd23126d8dc35cd05ced90daad97a11bf6bff62
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2016-10-19 13:13:03 -0200

    Fix tests

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ MOCK_MODULES = [
     "netifaces", "psutil",
     "lxml.html", "lxml.cssselect", "lxml",
     "praw",
-    "gi.repository", "dbus.mainloop.glib", "dbus",
+    "gi", "gi.repository", "dbus.mainloop.glib", "dbus",
     "pywapi", "basiciw",
     "i3pystatus.pulseaudio.pulse",
     "notmuch",
