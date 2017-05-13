3bb8de9574217753f10334b32974fb598a385049
enkore <public@enkore.de>
2017-05-13 15:58:29 +0200

    docs: highlight module name to use with register

--- docs/conf.py
+++ docs/conf.py
@@ -163,7 +163,7 @@ html_theme = "haiku"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
@@ -344,3 +344,6 @@ epub_copyright = '2013, Author'
 
 # If false, no index is generated.
 #epub_use_index = True
+
+def setup(app):
+    app.add_stylesheet('i3pystatus.css')
