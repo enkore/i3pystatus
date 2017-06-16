9d9836b2ec80c74f041fad0ba5b86758bd146e86
David Wahlstrom <david.wahlstrom@gmail.com>
2017-06-16 08:30:27 -0700

    OpenVPN: fix up unittests
    Cleaning up uneeded complexity and hacks in openvpn unittests.  Instead
    of relying on the system running the tests to have a specific PID
    running (previously, pid 1), I'm just mocking out the call and returning
    an arbitrary PID list.  This removes dependencies on the system running
    the test while still executing the majority of the function.

--- tests/test_openvpn_conffile.py
+++ tests/test_openvpn_conffile.py
@@ -3,27 +3,33 @@ Basic test for the openvpn conffile backend
 """
 
 import unittest
+from mock import patch
 from i3pystatus import openvpn
 from i3pystatus.openvpn import conffile
+import os
 
 
 class OpenvpnConffileTest(unittest.TestCase):
 
-    def test_openvpnconffile_up(self):
+    @patch('i3pystatus.openvpn.conffile.os', autospec=True)
+    def test_openvpnconffile_up(self, os):
         """
         Test status output of openvpn being "up"
         """
 
-        ovpnconffile = openvpn.Openvpn(vpn_name='testvpn', status_up='up', backend=conffile.File(conf_file='tests/ovpn_up.conf'))
+        openvpn.conffile.os.listdir.return_value = ['1', '11', '2', '22', '3', '33']
+        ovpnconffile = openvpn.Openvpn(vpn_name='testvpn', status_up='up', backend=conffile.File(conf_file='tests/ovpn.conf'))
         ovpnconffile.run()
         self.assertTrue(ovpnconffile.output['full_text'] == 'testvpn up')
 
-    def test_openvpnconffile_down(self):
+    @patch('i3pystatus.openvpn.conffile.os', autospec=True)
+    def test_openvpnconffile_down(self, os):
         """
         Test status output of openvpn being "down"
         """
 
-        ovpnconffile = openvpn.Openvpn(vpn_name='testvpn', status_down='down', backend=conffile.File(conf_file='tests/ovpn_down.conf'))
+        openvpn.conffile.os.listdir.return_value = ['11', '2', '22', '3', '33']
+        ovpnconffile = openvpn.Openvpn(vpn_name='testvpn', status_down='down', backend=conffile.File(conf_file='tests/ovpn.conf'))
         ovpnconffile.run()
         self.assertTrue(ovpnconffile.output['full_text'] == 'testvpn down')
 
