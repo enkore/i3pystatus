9098cbf99fa8617c3c0a0e2f637c98d8b6a5732d
qgnox <gnox.at@gmail.com>
2017-11-02 00:48:22 -0500

    PEP8 fixes

--- i3pystatus/cpu_freq.py
+++ i3pystatus/cpu_freq.py
@@ -33,18 +33,18 @@ class CpuFreq(IntervalModule):
             with open('/sys/devices/system/cpu/online') as f:
                 line = f.readline()
                 cpus_online = [int(cpu) for cpu in line.split(',') if cpu.find('-') < 0]
-                cpus_online_range = [cpu_range for cpu_range in line.split('-') if cpu_range.find('-') > 0 ]
+                cpus_online_range = [cpu_range for cpu_range in line.split('-') if cpu_range.find('-') > 0]
 
             for cpu_range in cpus_online_range:
-                cpus_online += [cpu for cpu in range(int(cpu_range[0]), int(cpu_range[1])+1)]
+                cpus_online += [cpu for cpu in range(int(cpu_range[0]), int(cpu_range[1]) + 1)]
 
-            mhz_values = [0.0 for cpu in range(max(cpus_online)+1)]
-            ghz_values = [0.0 for cpu in range(max(cpus_online)+1)]
+            mhz_values = [0.0 for cpu in range(max(cpus_online) + 1)]
+            ghz_values = [0.0 for cpu in range(max(cpus_online) + 1)]
             for cpu in cpus_online:
                 with open('/sys/devices/system/cpu/cpu{}/cpufreq/scaling_cur_freq'.format(cpu)) as f:
                     line = f.readline()
-                    mhz_values[cpu]=float(line.rstrip()) / 1000.0
-                    ghz_values[cpu]=float(line.rstrip()) / 1000000.0
+                    mhz_values[cpu] = float(line.rstrip()) / 1000.0
+                    ghz_values[cpu] = float(line.rstrip()) / 1000000.0
             cpus_offline = mhz_values.count(0.0)
         else:
             with open(self.file) as f:
@@ -55,8 +55,8 @@ class CpuFreq(IntervalModule):
         ghz = {"core{}g".format(key): "{0:1.2f}".format(value) for key, value in enumerate(ghz_values)}
         cdict = mhz.copy()
         cdict.update(ghz)
-        cdict['avg'] = "{0:4.3f}".format(sum(mhz_values) / (len(mhz_values)-cpus_offline))
-        cdict['avgg'] = "{0:1.2f}".format(sum(ghz_values) / (len(ghz_values)-cpus_offline),2)
+        cdict['avg'] = "{0:4.3f}".format(sum(mhz_values) / (len(mhz_values) - cpus_offline))
+        cdict['avgg'] = "{0:1.2f}".format(sum(ghz_values) / (len(ghz_values) - cpus_offline), 2)
         return cdict
 
     def run(self):
