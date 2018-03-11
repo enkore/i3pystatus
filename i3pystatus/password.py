d1a196531adf541c5515451003677f59bf57a79a
Stefan Devai <stedevai@gmail.com>
2018-03-11 02:04:36 +0100

    Copy to clipboard working with xclip

--- i3pystatus/password.py
+++ i3pystatus/password.py
@@ -1,4 +1,7 @@
 from i3pystatus import Module
+import random
+import string
+import subprocess
 
 class Password(Module):
     """
@@ -7,13 +10,24 @@ class Password(Module):
 
     settings = (
         "text",
+        ("length", "Length of the generated password"),
+        ("characters", "Character set used to generate a new password"),
         ("color", "HTML color hex code #RRGGBB"),
     )
 
     text = 'test'
+    length = 12
+    uppercase = True
+    digits = False
     color = None
 
+    on_leftclick = "generate"
+
     def init(self):
+        p = subprocess.Popen(['xclip', '-selection', 'c'],
+                             stdin=subprocess.PIPE, close_fds=True)
+        p.communicate(input='test'.encode('utf-8'))
+
         self.output = {
             "full_text": self.text
         }
@@ -21,4 +35,8 @@ class Password(Module):
             self.output["color"] = self.color
 
     def generate(self):
+        # chars = string.ascii_lowercase
+        # if self.uppercase: chars += string.ascii_uppercase
+        # if self.digits: chars += string.digits
+        # passwd = ''.join(random.SystemRandom.choice(chars) for x in range(self.length))
         pass
