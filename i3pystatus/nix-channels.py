744cf618d9448f6753b0a93184e96cafb98f396d
Matthieu Coudron <mcoudron@hotmail.com>
2022-04-11 00:25:40 +0200

    fixup

--- i3pystatus/nix-channels.py
+++ i3pystatus/nix-channels.py
@@ -1,7 +1,7 @@
 # import os
 import urllib.request
 from i3pystatus.core.util import internet, require
-from i3pystatus import IntervalModule, logger
+from i3pystatus import IntervalModule
 import datetime
 
 class NixChannels(IntervalModule):
@@ -11,10 +11,7 @@ class NixChannels(IntervalModule):
     """
     settings = (
         ("color", "Text color when online"),
-        ('color_offline', 'Text color when offline'),
-        ('channel', 'Name of the channel'),
-        ('format_online', 'Status text when online'),
-        # ('format_offline', 'Status text when offline'),
+        ('channel', 'Name of the channel. Must be tracked by https://channels.nix.gsc.io/'),
         ("interval", "Update interval"),
     )
 
@@ -29,11 +26,9 @@ class NixChannels(IntervalModule):
 
     @require(internet)
     def run(self):
-        url = f"https://channels.nix.gsc.io/{self.channel}/latest-url"
+        url = f"https://channels.nix.gsc.io/{self.channel}/history-url"
         conn = urllib.request.urlopen(url, timeout=30)
         advancement_timestamp = conn.read().decode().split()[-1]
-        # print()
-        # last_modified = conn.headers['last-modified']
         last_modified = datetime.datetime.fromtimestamp(int(advancement_timestamp))
         # outstr = today.strftime(self.prefixformat) + " "
         span = datetime.datetime.now() - last_modified
@@ -41,7 +36,7 @@ class NixChannels(IntervalModule):
         # if internet():
         self.output = {
             "color": self.color,
-            "full_text": "%s" % (span)
+            "full_text": f"{self.channel}: %s" % (span)
         }
 
 
