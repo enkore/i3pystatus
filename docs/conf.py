c86cb8ac520c2203fea76300831bab1367b69ea2
Alexandre Detiste <alexandre.detiste@gmail.com>
2024-01-21 22:41:35 +0100

    remove dependency on old mock

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
 # requires PyPI mock
-import mock
+import unittest.mock as mock
 
 MOCK_MODULES = [
     "alsaaudio",
